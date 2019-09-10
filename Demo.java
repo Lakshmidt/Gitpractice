d2e03672d88697647df886ce8bc5d947734d6311
Lakshmidt <lakshmidt2@gmail.com>
2019-09-10 10:15:06 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -11,5 +11,14 @@ public class Demo
 				System.out.print("* ");
 			System.out.println();
 		}
+		for(int i=5;i>0;i--)
+		{
+			for(int k=0;k<=5-i;k++)
+				System.out.print(" ");
+			for(int j=0;j<=i;j++)
+				System.out.print("* ");
+			System.out.println();
+		}
+
 	}
 }
